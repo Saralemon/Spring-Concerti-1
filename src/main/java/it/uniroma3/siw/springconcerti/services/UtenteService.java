1a270b8077b2a2aae3ef24d9e09f2a878481ca69
ArthexTheKing <aleandroroma1@gmail.com>
2022-08-12 21:27:20 +0200

    Configurazione della sicurezza

--- src/main/java/it/uniroma3/siw/springconcerti/services/UtenteService.java
+++ src/main/java/it/uniroma3/siw/springconcerti/services/UtenteService.java
@@ -1,5 +1,15 @@
 package it.uniroma3.siw.springconcerti.services;
 
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.springconcerti.repository.UtenteRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UtenteService {
+
+    @SuppressWarnings("unused")
+    private final UtenteRepository utenteRepository;
     
 }
