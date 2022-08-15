9eb43c89780ffd6924c735d1d9bf91d5d7af9540
ArthexTheKing <aleandroroma1@gmail.com>
2022-08-16 01:20:38 +0200

    Profilo (Admin)
    Stesura pagina di profilo per ora solo lato admin + component di supporto

--- src/main/java/it/uniroma3/siw/springconcerti/controllers/MainController.java
+++ src/main/java/it/uniroma3/siw/springconcerti/controllers/MainController.java
@@ -3,8 +3,11 @@ package it.uniroma3.siw.springconcerti.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import it.uniroma3.siw.springconcerti.components.CurrencyComponent;
+import it.uniroma3.siw.springconcerti.components.UserDetailsComponent;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -13,10 +16,20 @@ public class MainController {
 
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
+    private final UserDetailsComponent userDetailsComponent;
+    private final CurrencyComponent currencyComponent;
+
     @GetMapping(value = {"/", "/index"})
     public String index() {
         log.info("Richiesta GET /index");
         return "index";
+    }
+
+    @GetMapping("/profilo")
+    public String mostraProfilo(Model model) {
+        model.addAttribute("utente", this.userDetailsComponent.getUtenteAutenticato());
+        model.addAttribute("valuta", this.currencyComponent.getSimboloMoneta());
+        return "profilo";
     }
 
     @GetMapping("/admin/home")
